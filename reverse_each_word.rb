cf49efe2b61c2d753d23bdb72ae4e6ce4bf04866
Julian Patterson <julian.patterson@gmail.com>
2021-02-08 18:06:50 -0500

    commit tree b350352e1542bc82e5ceb1bf436a9354e31a236b

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+def reverse_each_word(sentence)
+
+    reversed_words = []
+
+    words = sentence.split(" ")
+
+        words.collect do |word|
+            reversed_words << word.reverse
+        end
+    
+    reversed_words.join(" ")
+
+end
